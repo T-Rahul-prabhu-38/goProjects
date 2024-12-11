945f4f6ec58e2b8c2ef86bef24064239e616192c
t rahul prabhu <trahulprabhu38@gmail.com>
2024-12-12 03:14:08 +0530

    done

--- goMovieBackend/main.go
+++ goMovieBackend/main.go
@@ -118,9 +118,9 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("movies/", createMovie).Methods("POST")
-	r.HandleFunc("movies/{id}", updateMovie).Methods("PUT")
-	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting port at port 8000")
 
